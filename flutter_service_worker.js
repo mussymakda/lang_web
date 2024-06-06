2413bcd221632ab18cf9c1f839a5c03990abee81
mussymakda <mustansir.makda@gmail.com>
2024-06-06 14:32:27 +0530

    heroku

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "37318d7cfcf57aff35e0a567a040959e
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "9d100cb7d75b3be3411cd2247931ca83",
+"flutter_bootstrap.js": "2622971556b3d96ec4eaed2be34bb169",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
