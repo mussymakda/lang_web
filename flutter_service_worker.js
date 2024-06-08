4d56ff76af061177b3e03a4e8f38925f1636031e
mussymakda <mustansir.makda@gmail.com>
2024-06-08 09:01:29 +0530

    cors

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1b936d458b9ca566bcafcfcc67a095c3
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f47e57b04144e6ca8253e4485c7aa030",
+"flutter_bootstrap.js": "5cefd82eb85cdefb7b05d973df675676",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
