e10dfe3a54f411e289a6c61514e529914ce65175
mussymakda <mustansir.makda@gmail.com>
2024-06-09 21:43:35 +0530

    URL error

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1b936d458b9ca566bcafcfcc67a095c3
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "d9858db103d7be1fd1ec38bcc3f1fddf",
+"flutter_bootstrap.js": "c7f140f6b9bfaaeec37987e5ea8feaa7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
