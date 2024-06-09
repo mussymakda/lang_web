007ec787891214e7656c86af8f3bebfc9af666d1
mussymakda <mustansir.makda@gmail.com>
2024-06-09 13:39:47 +0530

    packages updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3092924480"
+    serviceWorkerVersion: "2993180341"
   }
 });
